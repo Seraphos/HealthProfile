b1c26e4a7857b82a81cdead8fc1c455ad15ca906
Andrew Connelly <andrew.connelly@spgusa.com>
2018-06-28 16:40:28 -0400

    Add project files.

---
+++ HealthProfile/HealthProfile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace HealthProfile
+{
+    class HealthProfile
+    {
+        //variables
+        private int patientWeight, patientHeight, patientDoBMonth, patientDoBDay, patientDoBYear, userage;
+        private string patientLastName, patientFirstName;
+        private char patientGender;
+        private double userMaxHeartRate, lowerTargetHeartRatebound, upperTargetHeartRateBound;
+        int currentYear = DateTime.Now.Year;
+
+        //properties
+        public string PatientLastName {
+            get { return patientLastName; }
+            set { patientLastName = value;
+            } }
+        public int PatientFirstName{ get; set; }
+        public int PatientWeight { get; set;}
+        public int PatientDoBMonth {
+            get { return patientDoBMonth; }  set {
+                if (value > 12 || value < 1) throw new ArgumentOutOfRangeException("Cannot enter a number more than 12 or less than 1.");
+                else if (value >= 1 && value <=12) patientDoBMonth = value; } }
+        public int PatientDoBDay {
+            get { return PatientDoBDay; }
+            set { if (patientDoBDay < 1 || patientDoBDay > 31) throw new ArgumentOutOfRangeException("Cannot enter a number more than 31 or less than 1.");
+                else if (patientDoBDay <= 31 && patientDoBDay >= 1) patientDoBDay = value; } }
+        public int PatientDoBYear {
+            get { return PatientDoBYear; }
+            set {if (patientDoBYear < 1859 || patientDoBYear > currentYear) throw new 
+                        ArgumentOutOfRangeException("Cannot enter a number less than 1859 or more than {0}.", Convert.ToString(currentYear));
+                else if (patientDoBYear >= 1859 && patientDoBYear <= currentYear) patientDoBYear = value;}
+        } 
+        public char PatientGender {
+            get { return patientGender;}
+            set {if (patientGender == 'M' || patientGender == 'F' || patientGender == 'm' || patientGender == 'f') patientGender = value;
+                else throw new ArgumentOutOfRangeException("Please enter 'M' or 'F' only for gender.");
+            } }
+
+        public HealthProfile(int weight, int height, int monthDateOfBirth, int dayDateOfBirth, int yearDateOfBirth, string lastName, string firstName, char gender) {
+            patientWeight = weight; patientHeight = height; patientDoBMonth = monthDateOfBirth; patientDoBDay = dayDateOfBirth; patientDoBYear = yearDateOfBirth;
+            patientLastName = lastName; patientFirstName = firstName; patientGender = gender;
+               }//end constructor
+
+        public int CalculateAge(int methCurrentYear, int methYearOfBirth)
+        {
+            int userAge = methCurrentYear - methYearOfBirth;
+            return userAge;
+        }//end calculate age method
+
+        //this method depends on either asking the user their age or using the calculate age method
+        public double CalculateMaxumumHearRate(double methUserAge)
+        {
+            double userMaxHeartRate = 220 - methUserAge;
+            return userMaxHeartRate;
+        }//end calculate max heart rate method
+
+        public double CalculateLowerTargetHeartRateRangeBound(double methMaxHeartRate)
+        {
+            double lowerTargetHeartRateBound;
+            lowerTargetHeartRateBound = methMaxHeartRate * .5;
+            return lowerTargetHeartRateBound;
+        }//end calc calculate lower boundary target heart rate range
+
+        public double CalculateUpperTargetHeartRateRangeBound(double methMaxHeartRate)
+        {
+            double upperTargetHeartRateBound;
+            upperTargetHeartRateBound = methMaxHeartRate * .85;
+            return upperTargetHeartRateBound;
+        }//end calculate upper boundary target heart rate range
+
+        public double CalculateBMI(int patientHeight, int patientWeight)
+        {
+            double bodyMassIndex = (patientWeight * 703) / (patientHeight * patientHeight);
+            return bodyMassIndex;
+        }//end calculate BMI
+
+    }//end class
+}//end namespace
---
+++ HealthProfile/HealthProfileTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthProfile
+{
+    class HealthProfileTest
+    {
+        static void Main(string[] args)
+        {
+            int weight, height, doBMonth, doBDay, doBYear, age;
+            int localCurrentYear;
+            string patientLast, patientFirst;
+            char patientGender;
+            double maxHeartRate, maxTargHR, minTargHR, patientBMI;
+
+            localCurrentYear = DateTime.Now.Year;
+
+            Console.WriteLine("Enter your first name and last name in first/last order: ");
+            patientFirst = Console.ReadLine();
+            patientLast = Console.ReadLine();
+
+            Console.WriteLine("Enter your weight in pounds: ");
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your height in inches: ");
+            height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the day of your day of the month of birth, the month, and the year, in the listed order:");
+            Console.WriteLine("Day of birth: "); doBDay = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Month of birth as an integer: "); doBMonth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Year of birth: "); doBYear = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the first character of your gender (male/female): "); patientGender = Convert.ToChar(Console.Read());
+
+            HealthProfile randomMemberHealthProfile = new HealthProfile(weight, height, doBMonth, doBDay, doBYear, patientLast, patientFirst, patientGender);
+
+            //perform heart rate methods on random member profile
+            age = randomMemberHealthProfile.CalculateAge(localCurrentYear, doBYear);
+            maxHeartRate = randomMemberHealthProfile.CalculateMaxumumHearRate(age);
+            maxTargHR = randomMemberHealthProfile.CalculateUpperTargetHeartRateRangeBound(maxHeartRate);
+            minTargHR = randomMemberHealthProfile.CalculateLowerTargetHeartRateRangeBound(maxHeartRate);
+            patientBMI = randomMemberHealthProfile.CalculateBMI(height, weight);
+
+
+            Console.WriteLine("Be prepared for a deluge of personal data!");Console.ReadKey();
+            Console.WriteLine("Your age is: {0}", age); Console.WriteLine("Your first and last name are: {0} {1}", patientFirst, patientLast);Console.ReadKey();
+            Console.WriteLine("Your Date of Birth is: {0}/{1}/{2}", doBMonth, doBDay, doBYear);Console.ReadKey();
+            Console.WriteLine("Your gender is: {0}", patientGender);Console.ReadKey(); Console.WriteLine("Press another key to clear the screen..."); Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Your recommended Maximum heart rate is: {0}", maxHeartRate);Console.ReadKey();
+            Console.WriteLine("Your recommended target heart rate range is from {0} to {1}", minTargHR, maxHeartRate);Console.ReadKey();
+            Console.WriteLine("Your BMI is: {0}", patientBMI);Console.ReadKey();
+
+    }//end main method
+    }//end class
+}//end namespace
